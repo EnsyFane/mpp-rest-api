24d6ed3f86149ba3fbb7bfb9916b9af0fc086b4d
Stefan Tataran <fane_ecl@yahoo.com>
2021-05-14 16:21:23 +0300

    Added persistence

---
+++ RestApi/RestApi/Persistence/MockMatchRepository.cs
@@ -0,0 +1,62 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RestApi.Persistence
+{
+    public class MockMatchRepository : IMatchRepository
+    {
+        public void Add(Match entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Match> GetAll()
+        {
+            return new List<Match>
+            {
+                new Match
+                {
+                    Id = 1,
+                    HomeTeam = "home-team1",
+                    GuestTeam = "guest-team1",
+                    AvailableSeats = 8,
+                    MatchType = MatchType.Final,
+                    TicketPrice = 10.6f
+                },
+                new Match
+                {
+                    Id = 2,
+                    HomeTeam = "home-team2",
+                    GuestTeam = "guest-team2",
+                    AvailableSeats = 16,
+                    MatchType = MatchType.Quarterfinals,
+                    TicketPrice = 21.2f
+                }
+            };
+        }
+
+        public Match GetById(int id)
+        {
+            return new Match
+            {
+                Id = id,
+                HomeTeam = "home-team",
+                GuestTeam = "guest-team",
+                AvailableSeats = 8,
+                MatchType = MatchType.Final,
+                TicketPrice = 10.6f
+            };
+        }
+
+        public bool SaveChages()
+        {
+            return true;
+        }
+
+        public void Update(Match entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestApi/RestApi/Persistence/SqlMatchRepository.cs
@@ -0,0 +1,47 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestApi.Persistence
+{
+    public class SqlMatchRepository : IMatchRepository
+    {
+        private readonly RestApiDBContext _context;
+
+        public SqlMatchRepository(RestApiDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Match entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _context.Matches.Add(entity);
+        }
+
+        public IEnumerable<Match> GetAll()
+        {
+            return _context.Matches.ToList();
+        }
+
+        public Match GetById(int id)
+        {
+            return _context.Matches.FirstOrDefault(m => m.Id == id);
+        }
+
+        public bool SaveChages()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void Update(Match entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
