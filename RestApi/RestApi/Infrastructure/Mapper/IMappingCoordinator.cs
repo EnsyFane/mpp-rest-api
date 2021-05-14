902e8c1de1e639fd5cf6d2c27d92b8e22b4f477f
Stefan Tataran <fane_ecl@yahoo.com>
2021-05-14 15:17:26 +0300

    Initial commit

---
+++ RestApi/RestApi/Infrastructure/Mapper/IMappingCoordinator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RestApi.Infrastructure.Mapper
+{
+    public interface IMappingCoordinator
+    {
+        TDest Map<TSource, TDest>(TSource source);
+
+        IEnumerable<TDest> Map<TSource, TDest>(IEnumerable<TSource> source);
+
+        TDest Map<TSource, TDest>(TSource source, TDest dest);
+    }
+}
---
+++ RestApi/Tests/RestApi.Unit.Tests/Mapper/MappingCoordinatorTest.cs
@@ -0,0 +1,23 @@
+﻿using RestApi.Infrastructure.Mapper;
+using Xunit;
+
+namespace RestApi.Unit.Tests.Mapper
+{
+    public class MappingCoordinatorTest
+    {
+        [Fact]
+        public void InitializeMappings_ConfigurationIsValid()
+        {
+            var mappingCoordinator = new TestMappingCoordinator();
+            mappingCoordinator.AssertConfigurationIsValid();
+        }
+
+        private class TestMappingCoordinator : MappingCoordinator
+        {
+            public void AssertConfigurationIsValid()
+            {
+                Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+            }
+        }
+    }
+}
