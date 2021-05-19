23d07615026b5a736e9af65ddd5dab43b3764898
Stefan Tataran <fane_ecl@yahoo.com>
2021-05-20 00:32:31 +0300

    Added empty integration tests

---
+++ RestApi/Tests/RestApi.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RestApi.Integration.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
